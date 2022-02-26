287b36e81c1c41a85b4820f197cceb4d35e49a23
emilykidd3 <e.kidd61@yahoo.com>
2022-02-25 21:57:57 -0800

    added variables to store guessed words and available space, functions to get the current guess and update the array and add the letter into the board, added styling to the board to show the letter

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
+    let guessedWords = [[]]
+    let availableSpace = 1;
+
     const keys = document.querySelectorAll('.keyboard-row button')
 
     for (let i = 0; i <keys.length; i++) {
         keys[i].onclick = ({ target }) => {
-            const key = target.getAttribute("data-key");
+            const letter = target.getAttribute("data-key");
 
-            console.log(key)
+            updateGuessedWords(letter);
+        }
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1]
+    }
+
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr();
+
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter)
+
+            const availableSpaceEl = document.getElementById(String(1))
+            availableSpace = availableSpace + 1;
+
+            availableSpaceEl.textContent = letter;
         }
     }
 
