2761edbfd6416c3149463752ff004f19a1f8b36c
emilykidd3 <e.kidd61@yahoo.com>
2022-02-26 22:36:10 -0800

    added css library, and dynamic style to change tile color when guessed

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let availableSpace = 1;
 
     let word = "dairy"
+    let guessedWordCount = 0;
 
     const keys = document.querySelectorAll('.keyboard-row button')
 
@@ -34,6 +35,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const currentWord = currentWordArr.join('');
 
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200;
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = "rgb(58, 58, 60)"
+
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId)
+                letterEl.classList.add("animate_flipInX");
+                letterEl.style = `background-color:${tileColor}; border-color:${tileColor}`
+
+            }, interval);
+        })
+
+        guessedWordCount += 1;
+
         if (currentWord === word) {
             window.alert("Congratulations!")
         }
@@ -51,6 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let i = 0; i < 30; i++){
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate_animated");
             square.setAttribute("id", i + 1)
             gameBoard.appendChild(square);
         }
